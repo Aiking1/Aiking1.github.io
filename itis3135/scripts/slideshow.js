64142aaf7de63952d19c6d0f6f09ac01c2873424
Aiking1 <isaac03king@gmail.com>
2025-04-17 01:28:38 -0400

    slideshow editing

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -2,35 +2,44 @@
 
 
 
-let slideIndex = 1;
+let currentSlide = 0;
 
-const showSlides = function(n) {
-    const slides = $(".slides");
-    const thumbnails = $(".demo");
-  
-    if (n > slides.length) slideIndex = 1;
-    if (n < 1) slideIndex = slides.length;
-  
-    slides.hide();
-    thumbnails.removeClass("active");
-  
-    $(slides[slideIndex - 1]).show();
-    $(thumbnails[slideIndex - 1]).addClass("active");
-  };
+function showSlide(index) {
+  const totalSlides = slides.length;
+  // Loop back if index is out of bounds
+  if (index >= totalSlides) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = totalSlides - 1;
+  } else {
+    currentSlide = index;
+  }
+
+  slides.forEach((slide, i) => {
+    slide.classList.remove("slide-active");
+  });
+  slides[currentSlide].classList.add("slide-active");
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") currentSlide(1);
+    if (e.key === "ArrowLeft") currentSlide(-1);
+  });
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlide(currentSlide + n);
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function setSlide(index) {
+  showSlide(index);
 }
 
+// Optional: Auto advance
 $(document).ready(function() {
     showSlides(slideIndex);
 
     // Optional: Auto advance
-    setInterval(function () {
-        plusSlides(1);
+setInterval(function () {
+        currentSlide(1);
     }, 7000);
 });
